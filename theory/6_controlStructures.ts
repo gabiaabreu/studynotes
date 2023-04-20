c2f62681cffefe3d0267c3347e76d6ad627d2197
Gabriela Abreu <gabiabr3u@gmail.com>
2023-04-19 21:25:14 -0300

    feat: adds control structures content

---
+++ theory/6_controlStructures.ts
@@ -0,0 +1,19 @@
+/* 
+
+        CONTROL STRUCTURES 
+
+    LOOPS: While and for
+
+fun fact: any part of 'for' can be skipped, see below
+    for(; i < 5 ;) 
+that is possible and acts just like 
+    while(i < 5)
+
+breaking loops: use break!
+    if(value === 5) break;
+
+you could also use continue, to force the loop to stop there and to start a new iteration
+    if(value === 5) continue;
+s
+
+*/
